145e218336aace28244ee84f6be11fba9866723e
Nisrinaaacantikkk <124250026@student.upnyk.ac.id>
2026-05-07 11:58:16 +0700

    tambah file koneksi

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prediksi SNBP UPN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+<div class="card">
+
+<div class="title">🎓 Prediksi SNBP UPN</div>
+<div class="subtitle">Cek peluang kamu lolos SNBP dengan analisis data realistis</div>
+
+<a href="tambah.php">
+    <button>Mulai Sekarang</button>
+</a>
+
+</div>
+</div>
+
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","snbp");
+
+if(!$conn){
+    die("Koneksi gagal");
+}
+?>
