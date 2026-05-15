7571be16df99208c22d146bdab72c1bea6232a6a
Nisrinaaacantikkk <124250026@student.upnyk.ac.id>
2026-05-15 09:43:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Prediksi SNBP</title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="top-header">
+        <img src="logoupn.png" alt="Logo UPN">
+        <div>
+            <h2>Universitas Pembangunan Nasional “Veteran” Yogyakarta</h2>
+            <p>Prediksi Peluang Lolos SNBP</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <h1 class="title">
+                Prediksi <span>SNBP</span><br>
+                UPN Veteran Yogyakarta
+            </h1>
+
+            <p class="desc">
+                Analisis peluang lolos SNBP berdasarkan nilai rapor,
+                prestasi, dan data sekolah secara realistis.
+            </p>
+
+            <form action="biodata.php" method="POST">
+                <label>PRODI PILIHAN 1</label>
+                <select name="prodi1" required>
+                    <option value="">-- Pilih Program Studi --</option>
+
+                    <option value="Teknik Kimia D3|85.51">Teknik Kimia D3</option>
+                    <option value="Agribisnis|88.73">Agribisnis</option>
+                    <option value="Agroteknologi|88.67">Agroteknologi</option>
+                    <option value="Akuntansi|91.55">Akuntansi</option>
+                    <option value="Ekonomi Pembangunan|90">Ekonomi Pembangunan</option>
+                    <option value="Hubungan Masyarakat|89.26">Hubungan Masyarakat</option>
+                    <option value="Ilmu Administrasi Bisnis|90.72">Ilmu Administrasi Bisnis</option>
+                    <option value="Ilmu Hubungan Internasional|89.21">Ilmu Hubungan Internasional</option>
+                    <option value="Ilmu Komunikasi|89.21">Ilmu Komunikasi</option>
+                    <option value="Ilmu Tanah|86.46">Ilmu Tanah</option>
+                    <option value="Manajemen|91.61">Manajemen</option>
+                    <option value="Metalurgi|86.97">Metalurgi</option>
+                    <option value="Sistem Informasi|88.79">Sistem Informasi</option>
+                    <option value="Teknik Geofisika|86.48">Teknik Geofisika</option>
+                    <option value="Teknik Geologi|90.11">Teknik Geologi</option>
+                    <option value="Teknik Geomatika|86.46">Teknik Geomatika</option>
+                    <option value="Teknik Industri|90.42">Teknik Industri</option>
+                    <option value="Teknik Informatika|91.41">Teknik Informatika</option>
+                    <option value="Teknik Kimia|87.49">Teknik Kimia</option>
+                    <option value="Teknik Lingkungan|88.62">Teknik Lingkungan</option>
+                    <option value="Teknik Perminyakan|90.79">Teknik Perminyakan</option>
+                    <option value="Teknik Pertambangan|91.75">Teknik Pertambangan</option>
+                </select>
+
+                <label>PRODI PILIHAN 2</label>
+                <select name="prodi2" required>
+                    <option value="">-- Pilih Program Studi --</option>
+
+                    <option value="Teknik Kimia D3|85.51">Teknik Kimia D3</option>
+                    <option value="Agribisnis|88.73">Agribisnis</option>
+                    <option value="Agroteknologi|88.67">Agroteknologi</option>
+                    <option value="Akuntansi|91.55">Akuntansi</option>
+                    <option value="Ekonomi Pembangunan|90">Ekonomi Pembangunan</option>
+                    <option value="Hubungan Masyarakat|89.26">Hubungan Masyarakat</option>
+                    <option value="Ilmu Administrasi Bisnis|90.72">Ilmu Administrasi Bisnis</option>
+                    <option value="Ilmu Hubungan Internasional|89.21">Ilmu Hubungan Internasional</option>
+                    <option value="Ilmu Komunikasi|89.21">Ilmu Komunikasi</option>
+                    <option value="Ilmu Tanah|86.46">Ilmu Tanah</option>
+                    <option value="Manajemen|91.61">Manajemen</option>
+                    <option value="Metalurgi|86.97">Metalurgi</option>
+                    <option value="Sistem Informasi|88.79">Sistem Informasi</option>
+                    <option value="Teknik Geofisika|86.48">Teknik Geofisika</option>
+                    <option value="Teknik Geologi|90.11">Teknik Geologi</option>
+                    <option value="Teknik Geomatika|86.46">Teknik Geomatika</option>
+                    <option value="Teknik Industri|90.42">Teknik Industri</option>
+                    <option value="Teknik Informatika|91.41">Teknik Informatika</option>
+                    <option value="Teknik Kimia|87.49">Teknik Kimia</option>
+                    <option value="Teknik Lingkungan|88.62">Teknik Lingkungan</option>
+                    <option value="Teknik Perminyakan|90.79">Teknik Perminyakan</option>
+                    <option value="Teknik Pertambangan|91.75">Teknik Pertambangan</option>
+                </select>
+
+                <button type="submit" class="btn">
+                    Mulai Prediksi →
+                </button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ nilai.php
@@ -0,0 +1,120 @@
+<?php
+    session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $_SESSION['nama'] = $_POST['nama'];
+        $_SESSION['akreditasi'] = $_POST['akreditasi'];
+        $_SESSION['peringkat'] = $_POST['peringkat'];
+        $_SESSION['ranking'] = $_POST['ranking'];
+        $_SESSION['eligible'] = $_POST['eligible'];
+        $_SESSION['alumni'] = $_POST['alumni'];
+    } else {
+        header("Location: biodata.php");
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Input Nilai</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="top-header">
+        <img src="logoupn.png" alt="Logo UPN">
+        <div>
+            <h2>Universitas Pembangunan Nasional “Veteran” Yogyakarta</h2>
+            <p>Prediksi Peluang Lolos SNBP</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <div class="step">
+                <div class="active"></div>
+                <div class="active"></div>
+                <div class="active"></div>
+                <div></div>
+            </div>
+
+            <h2>Input Nilai Rapor</h2>
+
+            <form action="hasil.php" method="POST">
+                <div class="row">
+                    <div>
+                        <label>Semester 1</label>
+                        <input type="number" step="0.01" name="s1" min="1" max="100" required>
+                    </div>
+
+                    <div>
+                        <label>Semester 2</label>
+                        <input type="number" step="0.01" name="s2" min="1" max="100" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div>
+                        <label>Semester 3</label>
+                        <input type="number" step="0.01" name="s3" min="1" max="100" required>
+                    </div>
+
+                    <div>
+                        <label>Semester 4</label>
+                        <input type="number" step="0.01" name="s4" min="1" max="100" required>
+                    </div>
+                </div>
+
+                <label>Semester 5</label>
+                <input type="number" step="0.01" name="s5" min="1" max="100" required>
+                <h3 style="margin-top:30px;">
+                    Prestasi (Opsional)
+                </h3>
+
+                <div class="row">
+                    <div>
+                        <label>Prestasi 1</label>
+                        <select name="prestasi1">
+                            <option value="0">Tidak Ada</option>
+                            <option value="5">Kabupaten</option>
+                            <option value="8">Provinsi</option>
+                            <option value="12">Nasional</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Prestasi 2</label>
+                        <select name="prestasi2">
+                            <option value="0">Tidak Ada</option>
+                            <option value="5">Kabupaten</option>
+                            <option value="8">Provinsi</option>
+                            <option value="12">Nasional</option>
+                        </select>
+                    </div>
+                </div>
+
+                <label>Prestasi 3</label>
+                <select name="prestasi3">
+                    <option value="0">Tidak Ada</option>
+                    <option value="5">Kabupaten</option>
+                    <option value="8">Provinsi</option>
+                    <option value="12">Nasional</option>
+                </select>
+
+                <button type="submit" class="btn">
+                    Hitung Prediksi →
+                </button>
+            </form>
+
+            <button onclick="history.back()" class="btn-outline">
+                ← Kembali
+            </button>
+        </div>
+    </div>
+</body>
+</html>
